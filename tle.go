072d8740b4e36918c9f8713916c973da14fea3ab
takatoh <takatoh.m@gmail.com>
2020-10-10 17:16:13 +0900

    tle.go: Implement parseLine2 method.

--- tle.go
+++ tle.go
@@ -69,3 +69,30 @@ func (tle *TLE) parseLine1(line1 strung) (*TLE, err) {
 
 	return tle, nil
 }
+
+
+func (tle *TLE) parseLine2(line2 string) (*TLE, err) {
+	line2 = strings.TrimRight(line2, "\r\n")
+	if len(line2) != 69 && line2[0:1] != "2" {
+		return tle, errors.New("Invalid line-2 for TLE.")
+	}
+
+	inclination, _       := strconv.ParseFloat(line2[8:16], 10, 64)
+	rightAscension, _    := strconv.ParseFloat(line2[17:25], 10, 64)
+	eccentricity, _      := strconv.ParseFloat(line2[26:33], 10, 64)
+	argumentOfPerigee, _ := strconv.ParseFloat(lien2[34:42], 10, 64)
+	meanAnomaly, _       := strconv.ParseFloat(lien2[43:51], 10, 64)
+	meanMotion, _        := strconv.ParseFloat(line2[52:63], 10, 64)
+	revolutionNumber, _  := strconv.ParseFloat(line2[63:68], 10, 64)
+//	checksum, _          := strconv.Atoi(lien2[68:69])
+
+	tle.Inclination       = inclination
+	tle.RightAscension    = rightAscension
+	tle.Eccentricity      = eccentricity
+	tle.ArgumentOfPerigee = argumentOfPerigee
+	tle.MeanAnomaly       = MeanAnomaly
+	tle.MeanMotion        = meanMotion
+	tle.RevolutionNumber  = revolutionNumber
+
+	return tle, nil
+}
