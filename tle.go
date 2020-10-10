f8741bc439d9697e6bfeac5ef5d22f1a4ba7b3ca
takatoh <takatoh.m@gmail.com>
2020-10-10 18:00:39 +0900

    tle.go: Fix type, sysntax error and function call.

--- tle.go
+++ tle.go
@@ -34,7 +34,7 @@ func NewTLE() *TLE {
 }
 
 
-func (tle *TLE) parseLine1(line1 strung) (*TLE, err) {
+func (tle *TLE) parseLine1(line1 string) (*TLE, error) {
 	line1 = strings.TrimRight(line1, "\r\n")
 	if len(line1) != 69 && line1[0:1] != "1" {
 		return tle, errors.New("Invalid line-1 for TLE.")
@@ -71,26 +71,26 @@ func (tle *TLE) parseLine1(line1 strung) (*TLE, err) {
 }
 
 
-func (tle *TLE) parseLine2(line2 string) (*TLE, err) {
+func (tle *TLE) parseLine2(line2 string) (*TLE, error) {
 	line2 = strings.TrimRight(line2, "\r\n")
 	if len(line2) != 69 && line2[0:1] != "2" {
 		return tle, errors.New("Invalid line-2 for TLE.")
 	}
 
-	inclination, _       := strconv.ParseFloat(line2[8:16], 10, 64)
-	rightAscension, _    := strconv.ParseFloat(line2[17:25], 10, 64)
-	eccentricity, _      := strconv.ParseFloat(line2[26:33], 10, 64)
-	argumentOfPerigee, _ := strconv.ParseFloat(lien2[34:42], 10, 64)
-	meanAnomaly, _       := strconv.ParseFloat(lien2[43:51], 10, 64)
-	meanMotion, _        := strconv.ParseFloat(line2[52:63], 10, 64)
-	revolutionNumber, _  := strconv.ParseFloat(line2[63:68], 10, 64)
+	inclination, _       := strconv.ParseFloat(line2[8:16], 64)
+	rightAscension, _    := strconv.ParseFloat(line2[17:25], 64)
+	eccentricity, _      := strconv.ParseFloat(line2[26:33], 64)
+	argumentOfPerigee, _ := strconv.ParseFloat(line2[34:42], 64)
+	meanAnomaly, _       := strconv.ParseFloat(line2[43:51], 64)
+	meanMotion, _        := strconv.ParseFloat(line2[52:63], 64)
+	revolutionNumber, _  := strconv.ParseUint(line2[63:68], 10, 64)
 //	checksum, _          := strconv.Atoi(lien2[68:69])
 
 	tle.Inclination       = inclination
 	tle.RightAscension    = rightAscension
 	tle.Eccentricity      = eccentricity
 	tle.ArgumentOfPerigee = argumentOfPerigee
-	tle.MeanAnomaly       = MeanAnomaly
+	tle.MeanAnomaly       = meanAnomaly
 	tle.MeanMotion        = meanMotion
 	tle.RevolutionNumber  = revolutionNumber
 
